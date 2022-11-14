89faaae02914f5d7c7c610a2d900c14eb1570e1f
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-14 15:56:33 -0800

    created working assertion function

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Asserction Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual('yes', 'yes');
+assertEqual('yes', 'no');
+assertEqual(109, 392);
+assertEqual(789, 789);
