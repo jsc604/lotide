957c4f258df9e65e12641cd3eab97976ede786f1
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-15 21:44:48 -0800

    create flatten function to turn single level nested arrays into one array

---
+++ flatten.js
@@ -0,0 +1,38 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let value of arr1) {
+    if (arr1[value] !== arr2[value]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const flatten = function(input) {
+  let output = [];
+  for (let i of input) {
+    // console.log('i', i);
+    if (Array.isArray(i)) {
+      for (let j of i) {
+        // console.log('j', j);
+        output.push(j);
+      }
+    } else {
+      output.push(i);
+    }
+  }
+  return output;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, 2, [3], [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, [2, 3, 'yes'], 5, ['no']]), [1, 2, 3, 'yes', 5, 'no']);
+
+
