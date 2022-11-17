6cc9b3b3dcdff0ae6e31caec149a492fbe7cf87d
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-16 16:44:49 -0800

    create a function that displays letters and their  index position in the string

---
+++ letterPositions.js
@@ -0,0 +1,35 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      const key = sentence[i];
+      const position = i;
+      if (results[key]) {
+        results[key].push(position);
+      } else {
+        results[key] = [position];
+      }
+    }
+  }
+  return results;
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let value of arr1) {
+    if (arr1[value] !== arr2[value]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+console.log(letterPositions('lighthouse in the house'));
+assertArraysEqual(letterPositions('hello').e, [1]);
+assertArraysEqual(letterPositions('hello').l, [2, 3]);
