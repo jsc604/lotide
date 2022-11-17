efb167a284259cc814c67105873e5fb31093c56e
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-16 17:07:27 -0800

    create function that returns the key by passing the object and value as arguments

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  let genre;
+  for (let key in obj) {
+    // console.log('key', key);
+    // console.log('value', value);
+    // console.log(obj[key]);
+    if (obj[key] === value) {
+      genre = key;
+    }
+  }
+  return genre;
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
