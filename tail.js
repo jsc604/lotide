8cb04533295f1f94a2d5563c25bbe87550d3d3d8
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-21 12:59:48 -0800

    remove assert equal function and test code and exported tail function

--- tail.js
+++ tail.js
@@ -1,20 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   return array.slice(1);
 };
 
-const words = ['Yo Yo', 'Lighthouse', 'Labs'];
-const result = tail(words);
-assertEqual(words.length, 3);
-assertEqual(result[0], 'Lighthouse');
-assertEqual(result[1], 'Labs');
-assertEqual(result.length, 2);
-assertEqual(tail([]).length, 0);
-assertEqual(tail(['hello']).length, 0);
+module.exports = tail;
