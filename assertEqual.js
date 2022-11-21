847417346f9e2b0c4efc20f5f0080e1a71dff079
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-21 12:50:49 -0800

    created test file and exported tests and function to test file

--- assertEqual.js
+++ assertEqual.js
@@ -6,10 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-assertEqual('yes', 'yes');
-assertEqual('yes', 'no');
-assertEqual(109, 392);
-assertEqual(789, 789);
+module.exports = assertEqual;
