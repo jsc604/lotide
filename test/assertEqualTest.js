fc54c279bfb3f4d69b2c85382f07545442461a0e
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-21 12:53:13 -0800

    created test file and imported test code and function from assertEqual.js

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual =require('../assertEqual.js');
+
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual('yes', 'yes');
+assertEqual('yes', 'no');
+assertEqual(109, 392);
+assertEqual(789, 789);
