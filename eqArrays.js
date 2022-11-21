c13e73f0839ee201008bb87fa745674a110abc43
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-21 13:07:36 -0800

    remove assertEqual function and test code from eqArrays.js and exported to new test file for eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// const eqArrays = function(arr1, arr2) {
-//   if (arr1.length !== arr2.length) return false;
-//   for (let value of arr1) {
-//     if (arr1[value] !== arr2[value]) return false;
-//   }
-//   return true;
-// };
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
@@ -24,9 +8,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-
+module.exports = eqArrays;
