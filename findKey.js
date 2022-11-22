17591d4db75f8978e9a29053a8d97c2edbc8d26c
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-21 18:56:57 -0800

    refractored findKey.js and created npm test file for it

--- findKey.js
+++ findKey.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual');
-
 const findKey = (object, callback) => {
   for (let key in object) {
     if (callback(object[key])) return key;
@@ -7,21 +5,3 @@ const findKey = (object, callback) => {
 };
 
 module.exports = findKey;
-
-console.log(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)); // => "noma"
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), 'noma');
---
+++ test/findKeyTest.js
@@ -0,0 +1,17 @@
+const assert = require('chai').assert;
+const findKey = require('../findKey');
+
+const constellations = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+describe('#findKey', () => {
+  it('returns "noma" for findKey(constellations, x => x.stars === 2)', () => {
+    assert.strictEqual(findKey(constellations, x => x.stars === 2), 'noma');
+  });
+});
