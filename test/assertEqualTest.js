d3fd86e0b5e0a7452657ec7391beac9397e1f805
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-21 19:48:45 -0800

    refractored assertEqual.js and created npm test for it

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,8 +1,23 @@
-const assertEqual =require('../assertEqual.js');
+const assert = require('chai').assert;
+const assertEqual = require('../assertEqual.js');
 
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-assertEqual('yes', 'yes');
-assertEqual('yes', 'no');
-assertEqual(109, 392);
-assertEqual(789, 789);
+describe('#assertEqual', () => {
+  it('returns false for assertEqual("Lighthouse Labs", "Bootcamp")', () => {
+    assert.strictEqual(assertEqual('Lighthouse Labs', 'Bootcamp'), false);
+  });
+  it('returns true for assertEqual(1, 1)', () => {
+    assert.strictEqual(assertEqual(1, 1), true);
+  });
+  it('returns true for assertEqual("yes", "yes")', () => {
+    assert.strictEqual(assertEqual('yes', 'yes'), true);
+  });
+  it('returns false for assertEqual("yes", "no")', () => {
+    assert.strictEqual(assertEqual('yes', 'no'), false);
+  });
+  it('returns false for assertEqual(109, 392)', () => {
+    assert.strictEqual(assertEqual(109, 392), false);
+  });
+  it('returns true for assertEqual(789, 789))', () => {
+    assert.strictEqual(assertEqual(789, 789), true);
+  });
+});
